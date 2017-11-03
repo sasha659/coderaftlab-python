217331666ef2978546be0da29e2f7db704f2e4d2
sasha659 <agbfangirl@gmail.com>
2017-11-03 11:26:53 -0400

    Create astrology.py

---
+++ astrology.py
@@ -0,0 +1,21 @@
+epic_programmer_dict = {
+    'aquarius' : ['jan20 - feb18', 111],
+    'pisces' : ['feb19 - march20', 222],
+    'aries' : ['march21 - april19', 333],
+    'taurus' : ['april20 - may20', 444],
+    'gemini' : ['may21 - june20', 555],
+    'cancer' : ['june21 - july22', 666],
+    'leo' : ['july23 - aug22', 777],
+    'virgo' : ['aug23 - sept22', 888],
+    'libra' : ['sept23 - oct22', 999],
+    'scorpio' : ['oct23 - nov21', 1000],
+    'saggitarius' : ['nov22 - dec21', 1100],
+    'capricorn' : ['dec22 - jan19', 1200]
+                        }
+def searchAstro(peopleAstro):
+    try:
+        astroInfo = epic_programmer_dict[peopleAstro]
+        print 'astrology sign: ' + astroInfo[0]
+    except:
+        print "no info found"
+    
